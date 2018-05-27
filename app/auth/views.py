b70654c47c80003225851f7c73773ba14960f947
Monroe100 <michelle1hunnid@gmail.com>
2018-05-27 20:28:02 +0300

    created login route

--- app/auth/views.py
+++ app/auth/views.py
@@ -10,21 +10,20 @@ from flask_login import login_user,logout_user,login_required
 from .forms import RegistrationForm, LoginForm
 from .. import db
 
-# # registration route
-# @auth.route('/reqister',methods=['GET','POST'])
-# def register():
-#     form =RegistrationForm()
-#     if form.validate_on_submit():
-#         user =User(email=form.email.data,username=form.username.data,password=form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
+# registration route
+@auth.route('/reqister',methods=['GET','POST'])
+def register():
+    form =RegistrationForm()
+    if form.validate_on_submit():
+        user =User(email=form.email.data,username=form.username.data,password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
 
-#         return redirect(url_for('auth.login'))
-#         title='Register your Account'
-#     return render_template('auth/register.html',registration_form=form)
+        return redirect(url_for('auth.login'))
+        title='Register your Account'
+    return render_template('auth/register.html',registration_form=form)
 
-
-
+    
 # Login route
 @auth.route('/login',methods=['GET','POST'])
 def login():
