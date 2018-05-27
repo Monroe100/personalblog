82958e5677d3b1d927422e44ad856bbb48380385
Monroe100 <michelle1hunnid@gmail.com>
2018-05-27 17:45:16 +0300

    this commit cretaes the app instances

---
+++ manage.py
@@ -0,0 +1,27 @@
+
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User,Comments,Blog
+from  flask_migrate import Migrate, MigrateCommand
+from flask_login import LoginManager
+
+
+
+# Creating app instances
+# app = create_app('development')
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+#Migration
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db,Blog = Blog, Comments = Comments, User = User)
+
+
+if __name__ == '__main__':
+    manager.run()
