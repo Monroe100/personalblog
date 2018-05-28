c272e5f1ffc36857926112cf260657588a2ed10e
Monroe100 <michelle1hunnid@gmail.com>
2018-05-28 09:15:22 +0300

    initialized application

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -5,18 +5,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_simplemde import SimpleMDE
 
-# login_manager =LoginManager()
-# login_manager.session_protection='strong'
-# login_manager.login_view='auth.login'
+login_manager =LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view='auth.login'
 
-# bootstrap = Bootstrap()
-# db = SQLAlchemy()
-# simple = SimpleMDE()
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+simple = SimpleMDE()
 
-# def create_app(config_name):
+def create_app(config_name):
 
-#     # Initializing application
-#     app = Flask(__name__)
+    # Initializing application
+    app = Flask(__name__)
 
 #     # Creating the app configurations
 #     app.config.from_object(config_options[config_name])
