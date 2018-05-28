dac43d911a5f814fa8ef21ce79054bcb69dddd08
Monroe100 <michelle1hunnid@gmail.com>
2018-05-28 09:15:55 +0300

    create app configurations

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -18,8 +18,8 @@ def create_app(config_name):
     # Initializing application
     app = Flask(__name__)
 
-#     # Creating the app configurations
-#     app.config.from_object(config_options[config_name])
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
 #     # Initializing flask extensions
 #     bootstrap.init_app(app)
